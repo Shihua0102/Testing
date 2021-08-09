cbaca35e4e63137287c1529f34734d89bb9feded
shihua0102 <shihua0102@gmail.com>
2021-08-09 15:03:27 +0800

    testing title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel branch1</title>
+        <title>Laravel branch1 done</title>
 
 
         <!-- Fonts -->
